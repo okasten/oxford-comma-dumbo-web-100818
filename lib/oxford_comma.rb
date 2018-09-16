e50e2bfbf96f9dce5c500ad53f6c69ed5aa07e1d
Olivia Kasten <odkasten@gmail.com>
2018-09-16 16:11:37 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  i = 0
+  string = ""
+  if array.length < 2 
+    array[i]
+  elsif array.length < 3
+    "#{array[i]} and #{array[i+1]}"
+  else 
+    while i < array.length - 1
+      string << "#{array[i]}, "
+      i += 1 
+    end 
+    string << "and #{array[-1]}"
+  end 
 end
