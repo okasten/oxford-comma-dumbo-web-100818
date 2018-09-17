4449a113f817aad5ec0ee9096df9c39deaad795a
Olivia Kasten <odkasten@gmail.com>
2018-09-17 19:39:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,21 +1,16 @@
 def oxford_comma(array)
-  string = array.shift
-  last_word = array.pop
-  string << array.join(", ")
-  string << ", and #{last_word}"
-
-    
-  
-#  i = 0
- # string = ""
-  # array[i]
- # elsif array.length < 3
-  #  "#{array[i]} and #{array[i+1]}"
-#else 
- #   while i < array.length - 1
-  #    string << "#{array[i]}, "
-   #   i += 1 
-    #end 
-    #string << "and #{array[-1]}"
-  #end 
+ 
+  i = 0
+  string = ""
+  if array.length < 2
+    array[i] 
+  elsif array.length < 3
+    "#{array[i]} and #{array[i+1]}"
+  else 
+    while i < array.length - 1
+      string << "#{array[i]}, "
+      i += 1 
+    end 
+    string << "and #{array[-1]}"
+  end 
 end
