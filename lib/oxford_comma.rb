f8c4bc4f3a18c5048a577ec1b63944f58fbb71aa
Olivia Kasten <odkasten@gmail.com>
2018-09-17 19:31:18 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,15 +1,16 @@
 def oxford_comma(array)
-  i = 0
-  string = ""
-  if array.length < 2 
-    array[i]
-  elsif array.length < 3
-    "#{array[i]} and #{array[i+1]}"
-  else 
-    while i < array.length - 1
-      string << "#{array[i]}, "
-      i += 1 
-    end 
-    string << "and #{array[-1]}"
-  end 
+  
+  
+#  i = 0
+ # string = ""
+  # array[i]
+ # elsif array.length < 3
+  #  "#{array[i]} and #{array[i+1]}"
+#else 
+ #   while i < array.length - 1
+  #    string << "#{array[i]}, "
+   #   i += 1 
+    #end 
+    #string << "and #{array[-1]}"
+  #end 
 end
